379c6cc67f395647e615a4624db3a6c422f1b11a
tonyyonke <tonyvaldezyonke4@gmail.com>
2024-12-10 22:26:56 +0100

    Local ip setting

--- Assets/Demos/Pong/ClientManager.cs
+++ Assets/Demos/Pong/ClientManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class ClientManager : MonoBehaviour
 {
     public UDPService UDP;
-    public string ServerIP = "127.0.0.1";
+    public string ServerIP = "192.168.190.102";
     public int ServerPort = 25000;
 
     private float NextCoucouTimeout = -1;
